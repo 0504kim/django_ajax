a6c1b6e5ae4282ab20b03fed94a94418411e38c6
0504kim <minu0504@g.shingu.ac.kr>
2024-12-21 13:57:13 +0900

    first half

--- mysite/posts/views.py
+++ mysite/posts/views.py
@@ -1,7 +1,8 @@
+from urllib import request
 from django.shortcuts import render
 
 # Create your views here.
 
 # posts
-def posts(requests): # request객체는 HTTP요청의 모든 것(재료와 호출명)을 담고 있음
-    return render('posts')
+def posts(request): # request객체는 HTTP요청의 모든 것(재료와 호출명)을 담고 있음
+    return render(request, 'posts.html') # 확장자까지 붙이는 걸 권장하고 있음
